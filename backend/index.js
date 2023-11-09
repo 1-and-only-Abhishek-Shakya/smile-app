44116993a39ee6aa41d9210d60e6ebb354b69064
Abhishek kansotia <kansotiaabhishek007@gmail.com>
2023-11-09 19:37:29 +0530

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,204 @@
+const express = require("express");
+const app = express();
+const PORT = 8082;
+const sql = require("mssql");
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+
+app.use(express.json());
+
+app.use(
+  express.urlencoded({ extended: false, limit: 10000, parameterLimit: 5 })
+);
+//SQL Configuration
+const config = {
+  user: "sa",
+  password: "print@123",
+  server: `DESKTOP-R7UKJLG\\SQLEXPRESS`,
+  database: "tkt",
+  options: {
+    encrypt: false, // For secure connection to Azure SQL Database
+  },
+};
+
+// const config = {
+//   user: "abhi007_tkt",
+//   password: "sa1",
+//   server: `sql.bsite.net\\MSSQL2016`,
+//   database: "issues",
+//   options: {
+//     encrypt: false, // For secure connection to Azure SQL Database
+//   },
+// };
+
+// Transporter object for mail settings
+const transporter = nodemailer.createTransport({
+  host: "mail.printbaroda.com",
+  port: 99,
+  secure: false,
+  auth: {
+    user: "techsupport@printbaroda.com",
+    pass: "L9q%3ta1",
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
+
+// enable cors
+app.use(cors());
+app.options("*", cors());
+
+// Connect to database
+sql.connect(config, (err) => {
+  if (err) console.log(err);
+  else console.log("Connected to database !!");
+});
+
+// Testing sql query
+// app.get("/", (req, res) => {
+//   new sql.Request().query("select * FROM complaints", (err, result) => {
+//     if (err) console.log(err);
+//     else {
+//       res.send(result);
+//         const { recordsets } = result;
+//         res.send(recordsets[0][0]);
+//     }
+//   });
+// });
+
+// Selection of from to and content
+// const mailOptions = {
+//   from: "techsupport@printbaroda.com",
+//   to: "techsupport@printbaroda.com",
+//   subject: "Mail testing",
+//   text: "This is a test mail sent from techsupport to techsupport",
+// };
+
+// sendMail function to be called
+const sendMail = (remarks, email, user, company) => {
+  let mailOptions = {};
+  // console.log("Email = ",email,"Mail options = " ,mailOptions);
+  if (remarks) {
+    mailOptions = {
+      from: "techsupport@printbaroda.com",
+      to: `${email}`,
+      subject: `[SOLVED] Request fullfilled for ${company}`,
+      text: `
+  Dear ${user},\n \n
+  Your request is marked as completed.\n \n Regards,\nSmile Computers`,
+    };
+  } else {
+    console.log("Reached here. Mail ID is ", email);
+    mailOptions = {
+      from: "techsupport@printbaroda.com",
+      to: `${email}`,
+      subject: `Request submitted for ${company}`,
+      text: `
+    Dear ${user},\n\n
+    Your request for support is successfully submitted at Smile Computers.\nOur engineer will call / visit you shortly.\n\n Regards,\nSmile Computers`,
+    };
+  }
+  transporter.sendMail(mailOptions, (error, info) => {
+    // console.log(info);
+    if (error) {
+      console.log("Mail not sent", error);
+      console.log(mailOptions);
+      return 0; // 0 means mail is not sent
+    } else {
+      console.log("Mail sent");
+      return 1; // 1 means mail is  sent
+    }
+  });
+};
+
+//Formatting date before sending over API
+const dateFormatter = (arr) => {
+  const newArr = arr.map((item) => {
+    const strItem = JSON.stringify(item.date);
+    // console.log(strItem);
+    let newDate = strItem.replace("T00:00:00.000Z", "");
+    let finalDate = newDate.replace('"', "");
+    let date = finalDate.replace('"', "");
+    // console.log("new Date = ",newDate);
+    item.date = date;
+    return item;
+  });
+  return newArr;
+  //  console.log(newArr);
+};
+
+// Complaints endpoint to send all the complaints
+app.get("/complaints", (req, res) => {
+  new sql.Request().query("select * FROM complaints1", (err, result) => {
+    if (err) console.log(err);
+    else {
+      let { recordset } = result;
+      let finalData = dateFormatter(recordset);
+      // console.log("Following complaints sent: \n", finalData);
+      res.send(finalData);
+    }
+  });
+});
+
+// Receiving complaints
+// Define a route to handle the form submission
+app.post("/submit-complaint", (req, res) => {
+  // Extract data from the POST request
+  // console.log("Request body: ", req.body);
+  const { clientname, name, email, problem, issue, file } = req.body;
+
+  // Create a SQL query to insert the form data into the database
+  const sqlQuery = `INSERT INTO complaints1 (company, ussr, email ,problem, remarks, errFile) VALUES ('${clientname}', '${name}', '${email}' ,'${problem}', '${issue}', '${file}')`;
+  const request = new sql.Request();
+  request.query(sqlQuery, (err) => {
+    if (err) {
+      console.error("SQL query execution failed:", err);
+      return res.status(500).send("SQL query execution failed");
+    }
+    //sending 0 in sendMail to tell the function to send request submitted mail.  
+    sendMail(0, email, name, clientname);
+    res.status(200).send("Complaint submitted successfully");
+  });
+});
+// sql.close();
+
+//Updating complaints, set status from pending to completed
+app.patch("/status-update/", (req, res) => {
+  // const cardID = parseInt(req.params.RecordID);
+  const { remarks, ID, company, email, user } = req.body;
+  console.log(remarks, ID);
+  const sqlQuery = `update complaints1 set status = 1,remarks = '${remarks}' where RecordID = ${ID}`;
+  const request = new sql.Request();
+  request.query(sqlQuery, (err, result) => {
+    if (err) {
+      console.error("Query execution failed:", err);
+      return res.status(500).json({ message: "Query execution failed" });
+    } else if (result.rowsAffected[0] == 0) {
+      return res.status(404).json({ message: "Complaint not found" });
+    }
+    sendMail(remarks, email, user, company);
+    return res.status(200).json({ message: "Status set to completed!!" });
+  });
+});
+
+//Old code for patch
+// app.patch("/status-update/:RecordID", (req, res) => {
+//   const cardID = parseInt(req.params.RecordID);
+//   const { remarks } = req.body;
+// console.log(remarks);
+//   const sqlQuery = `update complaints1 set status = 1,remarks = '${remarks}' where RecordID = ${cardID}`;
+//   const request = new sql.Request();
+//   request.query(sqlQuery, (err, result) => {
+//     if (err) {
+//       console.error("Query execution failed:", err);
+//       return res.status(500).json({ message: "Query execution failed" });
+//     } else if (result.rowsAffected[0] == 0) {
+//       return res.status(404).json({ message: "Complaint not found" });
+//     }
+//     return res.status(200).json({ message: "Status set to completed!!" });
+//   });
+// });
+
+//Listen to port
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
---
+++ frontend/adminv2.js
@@ -0,0 +1,159 @@
+//Notes: add card number in db, work on updating the status from pending to completed.
+
+//'cardData' stores all the data coming from fetchData function
+
+// const { setTimeout } = require("timers/promises");
+let apiData; 
+async function pageStart() {
+    apiData = await fetchData();
+    console.log("Api Data:",apiData);
+    await renderCards(apiData);
+    count();    
+}
+
+// Function to fetch data from the API
+
+let URL = "http://localhost:8082/";
+//  let cardData;
+async function fetchData() {
+  try {
+    const response = await fetch(URL+"complaints");
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    //can provide additional alert that no data is received if cardData = 0
+    return 0;
+  }
+}
+
+async function statusUpdate(ID) {
+  try {
+    const response = await fetch(URL+`status-update/${ID}`, {
+      method: "PATCH", 
+    })
+    const data = await response.json(); 
+    alert(data.message);
+  } catch (error) {
+    console.log("Error updating data: ",error);
+  }
+}
+
+//function for counting number of records
+// function count() {
+//   const records = document.getElementById("count");  
+//   // const cardCounts = data.length(); 
+//   records.innerHTML += `<div>Total Records:</div>`;
+  
+// }
+
+// PATCH function for updating card status to completed 
+// Function to render cards based on the provided data
+async function renderCards(data) {
+  const cardList = document.getElementById("cardList");
+  cardList.innerHTML = "";
+    if(data == 0){
+        console.log("No Data found");
+    } 
+  data.forEach((card) => {
+    // added to match backend card status
+    let status;
+    if (card.status == 0) {
+      console.log(0);
+      status = "status-pending";
+    } else {
+      console.log(1);
+      status = "status-completed";
+    }
+    const cardHtml = `
+              <div class="col-md-3 col-sm-6 mb-3">
+              <div class="card border-primary mb-3">
+              <div class="card-header companyName ${status}">${card.company}</div>
+              <div class="card-body text-primary">
+                <h5 class="card-title">${card.problem}</h5>
+                <p class="card-text mb-1">
+                ${card.remarks}  
+                </p>
+      
+                <div class="mb-2">
+                  <small>Registered by ${card.ussr}</small>
+                </div>
+      
+                <div class="forFlex button text-end">
+                  <span
+                    ><svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      class="bi bi-clock"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"
+                      />
+                      <path
+                        d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z"
+                      /></svg
+                    ><small class="dates"> ${card.date}</small></span
+                  >
+      
+                  <span>
+                    <button type="button" onclick="statusUpdate(${card.RecordID})" class="btn btn-primary btn-sm" id="markButton">
+                      Issue Resolved
+                    </button></span
+                  >
+                </div>
+              </div>
+            </div>
+      
+              </div>
+          `;
+    cardList.innerHTML += cardHtml;
+  });
+}
+
+// Function to filter cards based on status
+async function filterCards(status) {
+  const filteredData = apiData.filter(
+    (card) => card.status === status || status === "All"
+  );
+  await renderCards(filteredData);
+}
+
+// Function to update card status (for demonstration purposes)
+async function updateStatus(cardNumber) {
+  const card = apiData.find((card) => card.RecordID === RecordID);
+  if (card) {
+    card.status = "Completed";
+    await renderCards(apiData);
+  }
+}
+
+// Event listeners
+document.getElementById("searchInput").addEventListener("input", function () {
+  const searchTerm = this.value.toLowerCase();
+  const filteredData = apiData.filter(
+    (card) =>
+      card.company.toLowerCase().includes(searchTerm) ||
+      card.ussr.toLowerCase().includes(searchTerm) ||
+      card.date.includes(searchTerm)
+  );
+  renderCards(filteredData);
+});
+
+document
+  .getElementById("filterAll")
+  .addEventListener("click", () => filterCards(0 && 1));
+document
+  .getElementById("filterPending")
+  .addEventListener("click", () => filterCards(0));
+document
+  .getElementById("filterCompleted")
+  .addEventListener("click", () => filterCards(1));
+
+
+// Initial card rendering
+pageStart();
+console.log(apiData);
+
